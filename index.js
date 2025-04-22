67955e5ff59157568f0bcd2d984ac9e0d0e31ee0
Pietrolio <pietromadioni224@gmail.com>
2025-04-22 19:59:25 +0200

    terminata la zona eventi, aggiungendo js per rendere il menu interattivo

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Happy developing ✨')
+//consente ad "activities-filter" di mantenere i suoi <li> arancioni e con testo bianco una volta cliccati
+//inoltre consente di visualizzare diversi menu di selezione in base all'attività scelta (EVENTS, MUSIC, etc., etc.)
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs   = document.querySelectorAll('.activities-filter li');
+    const panels = document.querySelectorAll('.panel');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+
+            //reset dei tabs
+            tabs.forEach(t => t.classList.remove('active'));
+            tab.classList.add('active');
+
+            //reset dei panel
+            panels.forEach(p => p.classList.remove('active'));
+
+            //mostra ciò che è collegato a questo panel
+            const id = tab.dataset.panel;
+            document.getElementById(id).classList.add('active');
+        });
+    });
+
+    //apre la prima voce
+    tabs[0].click();
+});
